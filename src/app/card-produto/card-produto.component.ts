df9d016ab0afa129fd6eb51d91216b20d51d03da
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2023-10-01 12:31:56 -0300

    commit

--- src/app/card-produto/card-produto.component.ts
+++ src/app/card-produto/card-produto.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 @Component({
   selector: 'app-card-produto',
   templateUrl: './card-produto.component.html',
   styleUrls: ['./card-produto.component.css']
 })
 export class CardProdutoComponent {
+  cards: any[] = [];
 
+  constructor(private http: HttpClient) { } // Injete o HttpClient no construtor
+
+  ngOnInit(): void {
+    this.http.get<any[]>('./assets/cards.json').subscribe(data => {
+      this.cards = data;
+    });
+  }
 }
